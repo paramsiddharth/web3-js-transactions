d827be92fd223edba33508183eaa627a4297a786
Param Siddharth <contact@paramsid.com>
2022-10-28 01:01:29 +0530

    Get Go version to work
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"math/big"
@@ -9,6 +10,9 @@ import (
 
 	"github.com/chenzhijie/go-web3"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/joho/godotenv"
 )
 
@@ -27,24 +31,33 @@ func main() {
 		log.Fatalln("Error: Environment variables not properly configured.")
 	}
 
-	web3, err := web3.NewWeb3(func() string {
+	getUrl := func() string {
 		envVar := os.Getenv("WEB3_PROVIDER")
 		if envVar != "" {
 			return envVar
 		}
 		return "ws://localhost:8545"
-	}())
+	}
+
+	ec, err := ethclient.Dial(getUrl())
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
+	}
+
+	web3, err := web3.NewWeb3(getUrl())
+	if err != nil {
+		panic(err)
 	}
 
 	count := 10
 	for i := 1; i <= count; i++ {
-		makeTransaction(web3, SELF, PEER, KEY, THEFT)
+		fmt.Printf("\n--- Iteration %d ---\n\n", i)
+		makeTransaction(ec, web3, SELF, PEER, KEY, THEFT)
 	}
 }
 
 func makeTransaction(
+	ec *ethclient.Client,
 	web3 *web3.Web3,
 	SELF string,
 	PEER string,
@@ -64,6 +77,7 @@ func makeTransaction(
 	spend := selfBal.Cmp(peerBal) >= 0
 	amount := new(big.Float).Mul(big.NewFloat(rand.Float64()), If(spend, selfBal, peerBal))
 	amt, _ := amount.Float64()
+	weis := toWei(web3, amt)
 	to := If(spend, PEER, SELF)
 	// from := If(spend, SELF, PEER)
 	key := If(spend, KEY, THEFT)
@@ -72,21 +86,56 @@ func makeTransaction(
 		panic(err)
 	}
 
-	web3.Eth.SetAccount(key)
-	tx, err := web3.Eth.SendRawTransaction(
-		common.HexToAddress(to),
-		toWei(web3, amt),
-		21000,
-		big.NewInt(int64(gasPrice)),
-		nil,
-	)
+	privKey, err := crypto.HexToECDSA(key[2:])
 	if err != nil {
 		panic(err)
 	}
 
-	// web3.Eth.NewEIP1559Tx()
+	pubKey := privKey.PublicKey
 
-	fmt.Printf("Transaction: %v\n", tx)
+	fromAdd := crypto.PubkeyToAddress(pubKey)
+	nonce, err := ec.PendingNonceAt(context.Background(), fromAdd)
+	if err != nil {
+		panic(err)
+	}
+
+	tx := types.NewTransaction(
+		nonce,
+		common.HexToAddress(to),
+		weis,
+		uint64(21000),
+		big.NewInt(int64(gasPrice)),
+		[]byte{},
+	)
+
+	signed, err := types.SignTx(tx, types.HomesteadSigner{}, privKey)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ec.SendTransaction(context.Background(), signed)
+	if err != nil {
+		panic(err)
+	}
+
+	tn, _, err := ec.TransactionByHash(context.Background(), signed.Hash())
+	if err != nil {
+		panic(err)
+	}
+
+	rct, err := ec.TransactionReceipt(context.Background(), tn.Hash())
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Amount: %v ETH\n", new(big.Float).Mul(amount, new(big.Float).SetInt64(func() int64 {
+		if spend {
+			return -1
+		}
+		return 1
+	}())))
+	fmt.Printf("Transaction: %v\n", signed.Hash())
+	fmt.Printf("Block: %v\n", rct.BlockNumber)
 
 	fmt.Println("--- After transaction ---")
 
